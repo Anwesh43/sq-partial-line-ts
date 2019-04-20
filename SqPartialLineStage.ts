ff6406439e34e8790a4c008f9c1b2551cffefdf7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 16:01:16 +0530

    adding SqPartialLine class

--- SqPartialLineStage.ts
+++ SqPartialLineStage.ts
@@ -183,3 +183,27 @@ class SPLNode {
         return this
     }
 }
+
+class SqPartialLine {
+
+    root : SPLNode = new SPLNode(0)
+    curr : SPLNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
