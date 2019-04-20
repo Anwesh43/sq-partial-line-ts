ddbaa4ce53480392a9de96537813b83986d65347
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 15:34:29 +0530

    adding State class

--- SqPartialLineStage.ts
+++ SqPartialLineStage.ts
@@ -95,3 +95,26 @@ class DrawingUtil {
         context.restore()
     }
 }
+
+class State {
+    dir : number = 0
+    prevScale : number = 0
+    scale : number = 0
+
+    update(cb : Function) {
+        this.scale += ScaleUtil.updateValue(this.scale, this.dir, lines, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
